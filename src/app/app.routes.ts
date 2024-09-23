58de91f2b041787c4f46ab7538b83cbf5d54bd80
sanron2703 <rohiniraju.g@gmail.com>
2024-09-23 22:19:32 +0530

    Landing page UI Implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contact-us',
+        loadComponent: () => import('./contact-us/contact-us.component').then(C => C.ContactUsComponent)
+    }
+];
